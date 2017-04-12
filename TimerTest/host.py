73d96d6ab722dc0809de0976923885e385f33ebf
Michael Zanga <zangamj@appstate.edu>
2017-04-12 15:13:57 -0400

    Added external interrupt 3-5 vectors. Python script prints raw time difference values.

--- TimerTest/host.py
+++ TimerTest/host.py
@@ -16,4 +16,4 @@ with serial.Serial() as ser:
 
 print('{:d}, {:d}, first capture'.format(state[0], pin[0]));
 for i in range(1,8):
-    print('{:d}, {:d}, {:f} seconds'.format(state[i], pin[i], ((float)(absTime[i]-absTime[i-1]))/16000000));
+    print('{:d}, {:d}, {:f} seconds    (raw {:d})'.format(state[i], pin[i], ((float)(absTime[i]-absTime[i-1]))/16000000, absTime[i]-absTime[i-1]));
--- TimerTest/main.c
+++ TimerTest/main.c
@@ -15,7 +15,7 @@ typedef struct {
 	unsigned long absTime; //Absolute time since the capture was started
 } timestamp;
 
-volatile timestamp timestamps[9]; //Only capturing 8 timestamps for this test
+volatile timestamp timestamps[9];
 volatile unsigned char tsIndex = 0; //index for the timestamp array
 
 //Sends a timestamp on the serial port as a char array
@@ -40,22 +40,28 @@ int main(void) {
 	UCSR0B = 0x18; //Enable transmitter and receiver
 
 	DDRB |= 0x40; //Set port B pin 6 as output
-	DDRH = 0x8; //Set port H bit 3 (OC4A) for output
+	DDRH |= 0x8; //Set port H bit 3 (OC4A) for output
 
 	TCCR4A = 0xC2; //Set OC4A to set on match, and half of fast PWM mode
 	TCCR4B = 0x18; //Other half of fast PWM mode
 	OCR4A = 0x7FFF; //Match on half max timer value
 	ICR4 = 0xFFFF; //Timer's top is max value
 
-	EICRA = 0x10; //Any edge on INT2 pin (19) triggers interrupt.
-	EIMSK |= 4; //Enable INT2
+	/* Uncommenting this block enables all 4 external interrupts
+	EICRA = 0x50; //Any edge on INT2 & INT3 pins (19 & 18) triggers interrupt.
+	EICRB = 0x5; //Any edge on INT4 & INT5 pins (2 & 3) triggers interrupt.
+	EIMSK |= 0x3C; //Enable INT2-5*/
+	EICRB = 4;
+	EIMSK |= 0x20;
 	EIFR = 0;
 
 	sei(); //Enable global interrupts.
 
 	TCCR5B = 0x6; //Clock on falling edge
 	TCCR4B |= 1; //Set clock source to no prescaling
-	_delay_ms(30);
+	
+	_delay_ms(32); //This removed the short time difference for the first interrupts
+	
 	while(1) {
 		PORTB |= 0x40;
 		_delay_ms(1);
@@ -63,7 +69,7 @@ int main(void) {
 		_delay_ms(1000);
 
 		if(tsIndex >= 8) {
-			EIMSK &= 0xFB;
+			EIMSK &= 0xC3;
 			tsIndex = 0;
 			sendTimestamps();
 		}
