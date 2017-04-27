4884690bd8090ff9aaf872de03f464cc99f841d5
Michael Zanga <zangamj@appstate.edu>
2017-04-27 16:25:12 -0400

    first attempt at circular buffer; works for a short while and then recieves garbage data

--- TimerTest/host.py
+++ TimerTest/host.py
@@ -1,19 +1,41 @@
 import serial
 
-state = [];
-pin = [];
-absTime = [];
+int2State = [];
+int3State = [];
+int4State = [];
+int5State = [];
+int2AbsTime = [];
+int3AbsTime = [];
+int4AbsTime = [];
+int5AbsTime = [];
 
 with serial.Serial() as ser:
     ser.port = 'COM3'
     ser.baudrate = 1000000
     ser.open()
-    for i in range(8):
-        state.append(int.from_bytes(ser.read(1), byteorder='little', signed=False))
-        pin.append(int.from_bytes(ser.read(1), byteorder='little', signed=False))
-        absTime.append(int.from_bytes(ser.read(4), byteorder='little', signed=False))
-    
+    while(1):
+        newState = int.from_bytes(ser.read(1), byteorder='little', signed=False)
+        newTime = int.from_bytes(ser.read(4), byteorder='little', signed=False)
+        newPin = int.from_bytes(ser.read(1), byteorder='little', signed=False)
 
-print('{:d}, {:d}, first capture'.format(state[0], pin[0]));
-for i in range(1,8):
-    print('{:d}, {:d}, {:f} seconds    (raw {:d})'.format(state[i], pin[i], ((float)(absTime[i]-absTime[i-1]))/16000000, absTime[i]-absTime[i-1]));
+        if(newPin == 19):
+            state = int2State;
+            absTime = int2AbsTime;
+        elif(newPin == 18):
+            state = int3State;
+            absTime = int3AbsTime;
+        elif(newPin == 2):
+            state = int4State;
+            absTime = int4AbsTime;
+        else:
+            state = int5State;
+            absTime = int5AbsTime;
+        state.append(newState);
+        absTime.append(newTime);
+        
+        if(len(state) == 1):
+            print('{:d}, {:d}, first capture'.format(newState, newPin));
+        else:
+            difference = newTime-absTime[-2];
+            print('{:d}, {:d}, {:f} seconds    (raw {:d})'
+                .format(newState, newPin, float(difference)/16000000, difference));
--- TimerTest/main.c
+++ TimerTest/main.c
@@ -9,14 +9,29 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+#define ARRAY_SIZE 64
+
 typedef struct {
 	unsigned char stateChange; //The detected state
-	unsigned char pinNo; //The pin the change occurred on
 	unsigned long absTime; //Absolute time since the capture was started
+	unsigned char pinNo; //The pin the change occurred on
 } timestamp;
 
-volatile timestamp timestamps[9];
-volatile unsigned char tsIndex = 0; //index for the timestamp array
+//Timestamp arrays
+volatile timestamp int2Timestamps[ARRAY_SIZE];
+volatile timestamp int3Timestamps[ARRAY_SIZE];
+volatile timestamp int4Timestamps[ARRAY_SIZE];
+volatile timestamp int5Timestamps[ARRAY_SIZE];
+//Next write index for the timestamp arrays
+volatile unsigned char int2Write = 0;
+volatile unsigned char int3Write = 0;
+volatile unsigned char int4Write = 0;
+volatile unsigned char int5Write = 0;
+//Next read index for the timestamp arrays
+unsigned char int2Read = 0;
+unsigned char int3Read = 0;
+unsigned char int4Read = 0;
+unsigned char int5Read = 0;
 
 //Sends a timestamp on the serial port as a char array
 void send(volatile timestamp * t) {
@@ -28,16 +43,17 @@ void send(volatile timestamp * t) {
 	}
 }
 
-//Function to write the contents of the timestamp array to the serial port.
-void sendTimestamps(void) {
-	for(char i = 0; i < 8; i++) send(timestamps+i);
-}
-
-//Setup I/O registers, and then flash PB6 with 1.25s period, duty cycle 20%
+//Setup I/O registers, interrupts, and timers, set pin numbers in timestamp arrays,
+//and then flash PB6 with 1.25s period, duty cycle 20%
 int main(void) {
+	unsigned int i;
+	for(i = 0; i < ARRAY_SIZE; i++) int2Timestamps[i].pinNo = 19;
+	for(i = 0; i < ARRAY_SIZE; i++) int3Timestamps[i].pinNo = 18;
+	for(i = 0; i < ARRAY_SIZE; i++) int4Timestamps[i].pinNo = 2;
+	for(i = 0; i < ARRAY_SIZE; i++) int5Timestamps[i].pinNo = 3;
+	
 	UCSR0A = 0x2; //U2X enable
-	//UBRR0 = 16; //Set baud rate to 115.2K
-	UBRR0 = 1;
+	UBRR0 = 1; //Set baud rate to 1M
 	UCSR0B = 0x18; //Enable transmitter and receiver
 
 	DDRB |= 0x40; //Set port B pin 6 as output
@@ -52,8 +68,8 @@ int main(void) {
 	EICRA = 0x50; //Any edge on INT2 & INT3 pins (19 & 18) triggers interrupt.
 	EICRB = 0x5; //Any edge on INT4 & INT5 pins (2 & 3) triggers interrupt.
 	EIMSK |= 0x3C; //Enable INT2-5*/
-	EICRA = 0x40;
-	EIMSK |= 0x08;
+	EICRA = 0x10;
+	EIMSK |= 0x04;
 	EIFR = 0;
 
 	sei(); //Enable global interrupts.
@@ -68,11 +84,22 @@ int main(void) {
 		_delay_ms(1);
 		PORTB &= 0xBF;
 		_delay_ms(1000);
-
-		if(tsIndex >= 8) {
-			EIMSK &= 0xC3;
-			tsIndex = 0;
-			sendTimestamps();
+		
+		while(int2Read != int2Write) {
+			send(int2Timestamps+int2Read);
+			int2Read = (int2Read+1) % ARRAY_SIZE;
+		}
+		while(int3Read != int3Write) {
+			send(int3Timestamps+int3Read);
+			int3Read = (int3Read+1) % ARRAY_SIZE;
+		}
+		while(int4Read != int4Write) {
+			send(int4Timestamps+int4Read);
+			int4Read = (int4Read+1) % ARRAY_SIZE;
+		}
+		while(int5Read != int5Write) {
+			send(int5Timestamps+int5Read);
+			int5Read = (int5Read+1) % ARRAY_SIZE;
 		}
 	}
 }
