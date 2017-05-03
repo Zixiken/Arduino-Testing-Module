1fa72059b9f84d6b485bbd9344133c21f83fa391
Michael Zanga <zangamj@appstate.edu>
2017-05-03 13:58:48 -0400

    Implemented python gui with tkinter. Test module now responds to serial input

--- TimerTest/host.py
+++ TimerTest/host.py
@@ -1,41 +1,177 @@
 import serial
+import threading
+import time
+from tkinter import *
 
-int2State = [];
-int3State = [];
-int4State = [];
-int5State = [];
-int2AbsTime = [];
-int3AbsTime = [];
-int4AbsTime = [];
-int5AbsTime = [];
+def sendCommands():
+    senseCommandA = 0
+    senseCommandB = 0
+    maskCommand = 0
 
-with serial.Serial() as ser:
-    ser.port = 'COM3'
-    ser.baudrate = 1000000
-    ser.open()
-    while(1):
+    global int2State
+    global int3State
+    global int4State
+    global int5State
+    global int2AbsTime
+    global int3AbsTime
+    global int4AbsTime
+    global int5AbsTime
+    int2State = []
+    int3State = []
+    int4State = []
+    int5State = []
+    int2AbsTime = []
+    int3AbsTime = []
+    int4AbsTime = []
+    int5AbsTime = []
+
+    if not ser.isOpen() and not serialConnect(port.get()): return
+    
+    selection = captEdge2.get()
+    if selection == options[0]: senseCommandB |= 0x3
+    elif selection == options[1]: senseCommandB |= 0x2
+    else: senseCommandB |= 0x1
+    selection = captEdge3.get()
+    if selection == options[0]: senseCommandB |= 0xC
+    elif selection == options[1]: senseCommandB |= 0x8
+    else: senseCommandB |= 0x4
+    
+    selection = captEdge18.get()
+    if selection == options[0]: senseCommandA |= 0xC0
+    elif selection == options[1]: senseCommandA |= 0x80
+    else: senseCommandA |= 0x40
+    selection = captEdge19.get()
+    if selection == options[0]: senseCommandA |= 0x30
+    elif selection == options[1]: senseCommandA |= 0x20
+    else: senseCommandA |= 0x10
+
+    if chan2.get(): maskCommand |= 0x10
+    if chan3.get(): maskCommand |= 0x20
+    if chan18.get(): maskCommand |= 0x8
+    if chan19.get(): maskCommand |= 0x4
+
+    ser.write(senseCommandA.to_bytes(1, byteorder='little', signed=False))
+    ser.write(senseCommandB.to_bytes(1, byteorder='little', signed=False))
+    ser.write(maskCommand.to_bytes(1, byteorder='little', signed=False))
+
+def serialReadLoop():
+    while(loop):
         newState = int.from_bytes(ser.read(1), byteorder='little', signed=False)
         newTime = int.from_bytes(ser.read(4), byteorder='little', signed=False)
         newPin = int.from_bytes(ser.read(1), byteorder='little', signed=False)
 
         if(newPin == 19):
-            state = int2State;
-            absTime = int2AbsTime;
+            state = int2State
+            absTime = int2AbsTime
         elif(newPin == 18):
-            state = int3State;
-            absTime = int3AbsTime;
+            state = int3State
+            absTime = int3AbsTime
         elif(newPin == 2):
-            state = int4State;
-            absTime = int4AbsTime;
+            state = int4State
+            absTime = int4AbsTime
         else:
-            state = int5State;
-            absTime = int5AbsTime;
-        state.append(newState);
-        absTime.append(newTime);
-        
+            state = int5State
+            absTime = int5AbsTime
+        state.append(newState)
+        absTime.append(newTime)
+
+        text.config(state=NORMAL)
         if(len(state) == 1):
-            print('{:d}, {:d}, first capture'.format(newState, newPin));
+            text.insert(END, '{:d}, {:d}, first capture\n'
+                .format(newState, newPin));
         else:
             difference = newTime-absTime[-2];
-            print('{:d}, {:d}, {:f} seconds    (raw {:d})'
+            text.insert(END, '{:d}, {:d}, {:f} seconds    (raw {:d})\n'
                 .format(newState, newPin, float(difference)/16000000, difference));
+        text.config(state=DISABLED)
+        text.see(END)
+
+def serialConnect(port):
+    ser.port = port
+    ser.baudrate = 1000000
+    try: ser.open()
+    except serial.SerialException as err:
+        print("Cannot open serial connection:")
+        print(err)
+        ser.close()
+        return False
+    time.sleep(1)
+    readThread.start()
+    return True
+
+options = ["rising edges", "falling edges", "both edges"]
+
+root = Tk()
+
+captEdge2 = StringVar(value=options[0])
+captEdge3 = StringVar(value=options[0])
+captEdge18 = StringVar(value=options[0])
+captEdge19 = StringVar(value=options[0])
+chan2 = BooleanVar()
+chan3 = BooleanVar()
+chan18 = BooleanVar()
+chan19 = BooleanVar()
+
+int2State = []
+int3State = []
+int4State = []
+int5State = []
+int2AbsTime = []
+int3AbsTime = []
+int4AbsTime = []
+int5AbsTime = []
+
+ser = serial.Serial()
+loop = True
+readThread = threading.Thread(target=serialReadLoop)
+
+f = Frame(root)
+Label(f, text="Serial Port:").pack(side=LEFT)
+port = Entry(f)
+port.insert(0, "COM3")
+port.pack(side=LEFT)
+f.pack()
+
+f = Frame(root)
+Checkbutton(f, variable=chan2).pack(side=LEFT)
+Label(f, text="Capture ").pack(side=LEFT)
+OptionMenu(f, captEdge2, *options).pack(side=LEFT)
+Label(f, text="on channel 2").pack(side=LEFT)
+f.pack()
+
+f = Frame(root)
+Checkbutton(f, variable=chan3).pack(side=LEFT)
+Label(f, text="Capture ").pack(side=LEFT)
+OptionMenu(f, captEdge3, *options).pack(side=LEFT)
+Label(f, text="on channel 3").pack(side=LEFT)
+f.pack()
+
+f = Frame(root)
+Checkbutton(f, variable=chan18).pack(side=LEFT)
+Label(f, text="Capture ").pack(side=LEFT)
+OptionMenu(f, captEdge18, *options).pack(side=LEFT)
+Label(f, text="on channel 18").pack(side=LEFT)
+f.pack()
+
+f = Frame(root)
+Checkbutton(f, variable=chan19).pack(side=LEFT)
+Label(f, text="Capture ").pack(side=LEFT)
+OptionMenu(f, captEdge19, *options).pack(side=LEFT)
+Label(f, text="on channel 19").pack(side=LEFT)
+f.pack()
+
+Button(root, text="Go!", command=sendCommands).pack()
+
+f = Frame(root)
+text = Text(f, state=DISABLED)
+text.pack(side=LEFT)
+scrollbar = Scrollbar(f, command=text.yview)
+scrollbar.pack(side=RIGHT, fill=Y)
+text.config(yscrollcommand=scrollbar.set)
+f.pack()
+
+root.mainloop()
+
+loop = False
+if readThread.isAlive(): readThread.join()
+if ser.isOpen(): ser.close()
--- TimerTest/main.c
+++ TimerTest/main.c
@@ -8,13 +8,13 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <util/twi.h>
 
 #define ARRAY_SIZE 64
 
 typedef struct {
 	unsigned char stateChange; //The detected state
 	unsigned long absTime; //Absolute time since the capture was started
-	unsigned char pinNo; //The pin the change occurred on
 } timestamp;
 
 //Timestamp arrays
@@ -34,71 +34,68 @@ unsigned char int4Read = 0;
 unsigned char int5Read = 0;
 
 //Sends a timestamp on the serial port as a char array
-void send(volatile timestamp * t) {
+void send(volatile timestamp * t, char pin) {
 	char * c = (char *)t;
 	for(char i = 0; i < sizeof(timestamp); i++) {
 		while(!(UCSR0A & 0x20));
 		UDR0 = *c;
 		c++;
 	}
+	while(!(UCSR0A & 0x20));
+	UDR0 = pin;
 }
 
 //Setup I/O registers, interrupts, and timers, set pin numbers in timestamp arrays,
-//and then flash PB6 with 1.25s period, duty cycle 20%
+//and then toggle pin 5 0.5s period 50% duty cycle.
 int main(void) {
-	unsigned int i;
-	for(i = 0; i < ARRAY_SIZE; i++) int2Timestamps[i].pinNo = 19;
-	for(i = 0; i < ARRAY_SIZE; i++) int3Timestamps[i].pinNo = 18;
-	for(i = 0; i < ARRAY_SIZE; i++) int4Timestamps[i].pinNo = 2;
-	for(i = 0; i < ARRAY_SIZE; i++) int5Timestamps[i].pinNo = 3;
-	
 	UCSR0A = 0x2; //U2X enable
 	UBRR0 = 1; //Set baud rate to 1M
 	UCSR0B = 0x18; //Enable transmitter and receiver
 
-	DDRB |= 0x40; //Set port B pin 6 as output
+	DDRB |= 0xC0; //Set port B pin 6 as output
 	DDRH |= 0x8; //Set port H bit 3 (OC4A) for output
 
 	TCCR4A = 0xC2; //Set OC4A to set on match, and half of fast PWM mode
 	TCCR4B = 0x18; //Other half of fast PWM mode
 	OCR4A = 0x7FFF; //Match on half max timer value
 	ICR4 = 0xFFFF; //Timer's top is max value
-
-	/* Uncommenting this block enables all 4 external interrupts
-	EICRA = 0x50; //Any edge on INT2 & INT3 pins (19 & 18) triggers interrupt.
-	EICRB = 0x5; //Any edge on INT4 & INT5 pins (2 & 3) triggers interrupt.
-	EIMSK |= 0x3C; //Enable INT2-5*/
-	EICRA = 0x10;
-	EIMSK |= 0x04;
-	EIFR = 0;
+	
+	//Generate square wave that isn't affected by interrupts
+	DDRE = 8; //Arduino pin 5, timer3 oca
+	PORTE = 8; //Turn on initially
+	TCCR3A = 0x82; //Fast PWM mode, clear oca on match
+	TCCR3B = 0x18;
+	ICR3 = 31250; //500ms period
+	OCR3A = 15625; //250ms match time
+	TCCR3B |= 4; //256 prescalar
 
 	sei(); //Enable global interrupts.
 
 	TCCR5B = 0x6; //Clock on falling edge
 	TCCR4B |= 1; //Set clock source to no prescaling
 	
-	_delay_ms(32); //This removed the short time difference for the first interrupts
-	
 	while(1) {
-		PORTB |= 0x40;
-		_delay_ms(1);
-		PORTB &= 0xBF;
-		_delay_ms(1000);
+		if(UCSR0A & 0x80) {
+			unsigned char in = UDR0;
+			if(in & 0xC0) EICRA = in;
+			else if(in & 0x03) EICRB = in;
+			else EIMSK = in;
+		}
 		
 		while(int2Read != int2Write) {
-			send(int2Timestamps+int2Read);
+			send(int2Timestamps+int2Read, 19);
 			int2Read = (int2Read+1) % ARRAY_SIZE;
 		}
 		while(int3Read != int3Write) {
-			send(int3Timestamps+int3Read);
+			send(int3Timestamps+int3Read, 18);
 			int3Read = (int3Read+1) % ARRAY_SIZE;
 		}
 		while(int4Read != int4Write) {
-			send(int4Timestamps+int4Read);
+			send(int4Timestamps+int4Read, 2);
 			int4Read = (int4Read+1) % ARRAY_SIZE;
 		}
 		while(int5Read != int5Write) {
-			send(int5Timestamps+int5Read);
+			send(int5Timestamps+int5Read, 3);
 			int5Read = (int5Read+1) % ARRAY_SIZE;
 		}
 	}
